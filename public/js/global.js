dbee937aec06b3cdfc3b1bf2f3f4111fd569b97b
jaeseunglee1991-source <jaeseung.lee1991@gmail.com>
2026-03-16 15:39:23 +0900

    Update global.js

--- public/js/global.js
+++ public/js/global.js
@@ -1,4 +1,3 @@
-// public/js/global.js
 const socket = io();
 var myUserId = "", myNickname = "", isAdmin = false;
 var myRoomId = "", selectedRoomId = "", selectedRoomRequiresPw = false;
@@ -7,6 +6,9 @@ var initialRouteHandled = false;
 
 const affinityEmoji = { 'SPICY':'🌶️', 'GREASY':'🍗', 'FRESH':'🥗', 'SALTY':'🧂', 'SWEET':'🍰', 'MINT_CHOCO':'🌿', 'PINEAPPLE':'🍍' };
 
-// 3D 엔진과 UI가 실시간으로 공유할 데이터
+// 3D 엔진과 UI가 실시간으로 공유할 전역 변수
 var entities = {};
 var meshMap = {};
+var canvas, scene, camera, renderer, mainLight;
+var environmentGroup;
+var currentThemeRoom = null;
