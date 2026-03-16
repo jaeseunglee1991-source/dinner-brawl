de5cdd7e7e76445eab327f8971e2c024f87cf0d2
jaeseunglee1991-source <jaeseung.lee1991@gmail.com>
2026-03-16 15:48:09 +0900

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,7 @@ const pool = new Pool({
 async function initDB() {
     try {
         await pool.query(`CREATE TABLE IF NOT EXISTS users (id VARCHAR(50) PRIMARY KEY, pw VARCHAR(100) NOT NULL, nickname VARCHAR(50) NOT NULL, is_admin BOOLEAN DEFAULT FALSE)`);
-        await pool.query(`INSERT INTO users (id, pw, nickname, is_admin) VALUES ('root', 'Skfls1223@', '시스템관리자', TRUE) ON CONFLICT (id) DO NOTHING`);
+        await pool.query(`INSERT INTO users (id, pw, nickname, is_admin) VALUES ('root', 'Test12#$', '시스템관리자', TRUE) ON CONFLICT (id) DO NOTHING`);
         console.log("✅ PostgreSQL DB 연결 및 초기화 완료!");
     } catch (err) { console.error("❌ DB 초기화 에러:", err); }
 }
