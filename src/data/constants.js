782ffc6f2d1686a2b5c41365d044c84dd96105f8
jaeseunglee1991-source <jaeseung.lee1991@gmail.com>
2026-03-16 14:41:17 +0900

    Create constants.js

---
+++ src/data/constants.js
@@ -0,0 +1,6 @@
+const AFFINITIES = ['SPICY', 'GREASY', 'FRESH', 'SALTY', 'SWEET', 'MINT_CHOCO', 'PINEAPPLE'];
+const SKILLS = [ /* 기존 배열 복사 */ ];
+const JOBS = [ /* 추가하셨던 직업 10종 배열 복사 */ ];
+const GRADES = [ /* 추가하셨던 등급 5종 배열 복사 */ ];
+
+module.exports = { AFFINITIES, SKILLS, JOBS, GRADES };
